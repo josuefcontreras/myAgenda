9bff9daecc0e6086538f157c8be4b762fe160421
Josue Contreras <josue.contreras21@gmail.com>
2020-07-24 02:04:49 -0400

    add Connection and Agenda classes

---
+++ MyAgenda/Agenda.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAgenda
+{
+    class Agenda
+    {
+        private SqliteConnection cn = null; // Para 
+        private SqliteCommand cmd = null; // Para 
+        private SqliteDataReader reader = null; // Para 
+        private DataTable table = null; // Para
+
+        public bool insertar(string nombre, string telefono)
+        {
+            return false;
+        }
+        public DataTable consultar ()
+        {
+            return new DataTable();
+        }
+        public bool eliminar(int id)
+        {
+            return false;
+        }
+        public DataTable filtrar(string filtro)
+        {
+            return new DataTable();
+        }
+        public bool actualizar(int id, string nombre, string telefono)
+        {
+            return false;
+        }
+
+        private void nombresColumnas()
+        {
+
+        }
+    }
+}
---
+++ MyAgenda/Conexion.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyAgenda
+{
+    class Conexion
+    {
+       public static SqliteConnection connectar()
+        {
+            string db = Application.StartupPath + "\\myagenda.db";
+            SqliteConnection cn = new SqliteConnection($"Data Source={db}");
+            return cn;
+        }
+    }
+}
